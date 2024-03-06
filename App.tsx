e7777e1bf46928f21752ec19422f1fb05557a221
Becky <bjentwistle@gmail.com>
2024-03-06 15:12:42 +0000

    added provider Google maps

--- App.tsx
+++ App.tsx
@@ -1,11 +1,10 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import MapView, {PROVIDER_GOOGLE} from 'react-native-maps';
+import { StyleSheet, View, Dimensions } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <MapView style={styles.map} provider={PROVIDER_GOOGLE} />
     </View>
   );
 }
@@ -13,8 +12,9 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  },
+  map: {
+    width: '100%',
+    height: '100%',
   },
 });
