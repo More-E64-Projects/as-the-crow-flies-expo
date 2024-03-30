766ad1bcc663983abcb39e0f5de4e34428a7dc6d
Becky <bjentwistle@gmail.com>
2024-03-30 15:19:22 +0000

    added homescreen with navigation to gamescreen

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -4,6 +4,7 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { AppProvider } from './AppContext';
 import GameScreen from './screens/GameScreen';
 import PauseScreen from './screens/PauseScreen';
+import HomeScreen from './screens/HomeScreen';
 
 const Stack = createNativeStackNavigator();
 
@@ -13,8 +14,10 @@ function App() {
     <AppProvider>
       <NavigationContainer>
         <Stack.Navigator screenOptions={{ headerShown: false }}>
+          <Stack.Screen name="Home" component={HomeScreen} />
           <Stack.Screen name="Game" component={GameScreen} />
           <Stack.Screen name="Pause" component={PauseScreen} />
+          
         </Stack.Navigator>
       </NavigationContainer>
     </AppProvider>
