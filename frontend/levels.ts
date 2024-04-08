d87984266b941873553743ff57f5bfe04410c717
Alex <alexgjohn@outlook.com>
2024-04-08 14:26:14 +0100

    added images to each location that show upon successful location

--- frontend/levels.ts
+++ frontend/levels.ts
@@ -4,6 +4,7 @@ type Location = {
     name: string;
     coordinates: LatLng;
     positionInList: number;
+    imageUrl: string
 };
 
 export type Level = {
@@ -20,7 +21,8 @@ export const edinburghLandmarks: Level = {
                 latitude: 55.9486,
                 longitude: -3.1999
             },
-            positionInList: 1
+            positionInList: 1,
+            imageUrl: "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c1/Edinburgh_Castle_from_the_south_east.JPG/500px-Edinburgh_Castle_from_the_south_east.JPG"
         },
         {
             name: "Arthur's Seat",
@@ -28,7 +30,8 @@ export const edinburghLandmarks: Level = {
                 latitude: 55.9444,
                 longitude: -3.1619
             },
-            positionInList: 2
+            positionInList: 2,
+            imageUrl: "https://upload.wikimedia.org/wikipedia/commons/thumb/0/0f/Arthur%27s_Seat%2C_Edinburgh.JPG/544px-Arthur%27s_Seat%2C_Edinburgh.JPG"
         },
         {
             name: "Royal Botanic Garden Edinburgh",
@@ -36,7 +39,8 @@ export const edinburghLandmarks: Level = {
                 latitude: 55.9653,
                 longitude: -3.2098
             },
-            positionInList: 3
+            positionInList: 3,
+            imageUrl: "https://upload.wikimedia.org/wikipedia/commons/thumb/f/f8/Palm_House%2C_Royal_Botanic_Garden_Edinburgh.JPG/440px-Palm_House%2C_Royal_Botanic_Garden_Edinburgh.JPG"
         },
         {
             name: "National Museum of Scotland",
@@ -44,7 +48,8 @@ export const edinburghLandmarks: Level = {
                 latitude: 55.9479,
                 longitude: -3.1898
             },
-            positionInList: 4
+            positionInList: 4,
+            imageUrl: "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b2/Museum_of_Scotland.jpg/500px-Museum_of_Scotland.jpg"
         },
         {
             name: "Calton Hill",
@@ -52,7 +57,8 @@ export const edinburghLandmarks: Level = {
                 latitude: 55.9544,
                 longitude: -3.1825
             },
-            positionInList: 5
+            positionInList: 5,
+            imageUrl: "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a5/Edinburgh_Calton_Hill.jpg/576px-Edinburgh_Calton_Hill.jpg"
         }
     ]
 };
