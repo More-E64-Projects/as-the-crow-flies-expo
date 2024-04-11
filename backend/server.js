4f17adcf66fdb2a019e4ded45b599e7b5208136e
Becky <bjentwistle@gmail.com>
2024-04-11 18:27:55 +0100

    added mongodb atlas to server

---
+++ backend/server.js
@@ -0,0 +1,26 @@
+require("dotenv").config();
+
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+
+const mongoUrl = process.env.DB_URL;
+
+mongoose
+  .connect(mongoUrl)
+  .then(() => {
+    console.log("Database connected");
+  })
+  .catch(console.err);
+
+app.get("/", (req, res) => {
+  res.send({ status: "Server started" }).catch((err) => {
+    console.error(err);
+    res.status(500);
+    res.json({ status: 500, error: err });
+  });
+});
+
+app.listen(9000, function () {
+  console.log(`Listening on port ${this.address().port}`);
+});
