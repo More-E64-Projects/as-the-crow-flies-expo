8c5398b5af8fda3a913f16fd3f4b6166c4f3910f
Becky <bjentwistle@gmail.com>
2024-04-16 23:06:51 +0100

    added create router file

---
+++ backend/create_router.js
@@ -0,0 +1,77 @@
+const ObjectID = require("mongodb").ObjectID; //imports the ObjectID object from the mongodb package.
+const express = require("express");
+const createRouter = function (collection) {
+  const router = express.Router();
+
+  router.get("/", (req, res) => {
+    collection
+      .find()
+      .toArray()
+      .then((docs) => res.json(docs))
+      .catch((err) => {
+        console.error(err);
+        res.status(500);
+        res.json({ status: 500, error: err });
+      });
+  });
+
+  router.get("/:id", (req, res) => {
+    const id = req.params.id;
+    collection
+      .findOne({ _id: ObjectID(id) })
+      .then((doc) => res.json(doc))
+      .catch((err) => {
+        console.error(err);
+        res.status(500);
+        res.json({ status: 500, error: err });
+      });
+  });
+
+  router.post("/", (req, res) => {
+    const newData = req.body;
+    collection
+      .insertOne(newData)
+      .then((result) => {
+        res.json(result.ops[0]);
+      })
+      .catch((err) => {
+        console.error(err);
+        res.status(500);
+        res.json({ status: 500, error: err });
+      });
+  });
+
+  router.put("/:id", (req, res) => {
+    const id = req.params.id;
+    const updatedData = req.body;
+    delete updatedData._id;
+
+    collection
+      .updateOne({ _id: ObjectID(id) }, { $set: updatedData })
+      .then((result) => {
+        res.json(result);
+      })
+      .catch((err) => {
+        res.status(500);
+        res.json({ status: 500, error: err });
+      });
+  });
+
+  router.delete("/:id", (req, res) => {
+    const id = req.params.id;
+    collection
+      .deleteOne({ _id: ObjectID(id) })
+      .then((result) => {
+        res.json(result);
+      })
+      .catch((err) => {
+        console.error(err);
+        res.status(500);
+        res.json({ status: 500, error: err });
+      });
+  });
+
+  return router;
+};
+
+module.exports = createRouter;
